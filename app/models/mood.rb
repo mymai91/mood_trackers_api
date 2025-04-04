eb02ea49fd022cad61a0b3831f2f64743dc690aa
My Mai <my.mai@we-are-mea.com>
2025-04-05 11:07:59 +1300

    Create Mood model and test case to validation mood model

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -11,7 +11,7 @@
 #  updated_at :datetime         not null
 #
 class Mood < ApplicationRecord
-  enum emotion: { not_good_at_all: "not_good_at_all", a_bit_meh: "a_bit_meh", pretty_good: "pretty_good", felling_greate: "felling_greate" }
+  enum :emotion, { not_good_at_all: "not_good_at_all", a_bit_meh: "a_bit_meh", pretty_good: "pretty_good", felling_greate: "felling_greate" }
 
   validates :emotion, presence: true
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true
--- spec/models/mood_spec.rb
+++ spec/models/mood_spec.rb
@@ -13,5 +13,23 @@
 require 'rails_helper'
 
 RSpec.describe Mood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mood) { create(:mood) }
+  let(:invalid_mood) { build(:mood, emotion: nil) }
+  let(:invalid_ip_address) { build(:mood, ip_address: "invalid_ip") }
+
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(mood).to be_valid
+    end
+
+    it "it is invalid if emotion is nil" do
+      expect(invalid_mood).to_not be_valid
+      expect(invalid_mood.errors[:emotion]).to include("can't be blank")
+    end
+
+    it "it is invalid if ip_address is invalid" do
+      expect(invalid_ip_address).to_not be_valid
+      expect(invalid_ip_address.errors[:ip_address]).to include("Must be valid IP address")
+    end
+  end
 end
