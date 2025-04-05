4b1e0a69931a836200b967a0169be37c8e109a9e
My Mai <my.mai@we-are-mea.com>
2025-04-05 15:03:14 +1300

    Add signout button to admin

--- app/controllers/admin_users/sessions_controller.rb
+++ app/controllers/admin_users/sessions_controller.rb
@@ -24,6 +24,10 @@ class AdminUsers::SessionsController < Devise::SessionsController
     admin_root_path
   end
 
+  def after_sign_out_path_for(resource)
+    new_admin_user_session_path
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
